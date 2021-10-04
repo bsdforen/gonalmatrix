288fc610754bfdb67bcd186e229f74f1e69d6000
Yamagi <yamagi@yamagi.org>
2021-10-04 14:40:25 +0200

    Basic error handling:
    * Die with nice and end user friendly error messages.
    * Add a varpanic() wrapper which allows to panic with formated messages.

--- cmd/gonalmatrix/main.go
+++ cmd/gonalmatrix/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+// ----
+
+// Wrapper function that allows to panic() with a formatted string.
+func varpanic(format string, args ...interface{}) {
+	msg := fmt.Sprintf("ERROR: "+format+"\n", args...)
+	panic(msg)
+}
+
+// ----
+
 func main() {
-	fmt.Println("Hallo Welt!")
+	// Die with nicer error messages.
+	defer func() {
+		if msg := recover(); msg != nil {
+			fmt.Fprintf(os.Stderr, "%v", msg)
+		}
+	}()
 }
