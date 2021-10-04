f3570716fd38f5d5bbd8bf812e9397843d094f69
Yamagi <yamagi@yamagi.org>
2021-10-04 15:18:07 +0200

    Add version number brinting through `-v`.

--- cmd/gonalmatrix/main.go
+++ cmd/gonalmatrix/main.go
@@ -8,6 +8,12 @@ import (
 
 // ----
 
+// Version number.
+const VERSION_MAJOR int = 0
+const VERSION_MINOR int = 1
+
+// ----
+
 // Wrapper function that allows to panic() with a formatted string.
 func varpanic(format string, args ...interface{}) {
 	msg := fmt.Sprintf("ERROR: "+format+"\n", args...)
@@ -26,7 +32,13 @@ func main() {
 
 	// Command line arguments.
 	var cfgptr = flag.String("c", "gonalmatrix.ini", "Config file")
+	var verptr = flag.Bool("v", false, "Print version number")
 	flag.Parse()
+
+	if *verptr {
+		fmt.Printf("gonalmatrix v%v.%v\n", VERSION_MAJOR, VERSION_MINOR)
+		os.Exit(0)
+	}
 
 	if stat, err := os.Stat(*cfgptr); err == nil {
 		if stat.IsDir() {
@@ -36,6 +48,9 @@ func main() {
 		varpanic("No such file: %v", *cfgptr)
 	}
 	cfgfile := *cfgptr
+	
+	// Print startup message
+	fmt.Printf("This is gonalmatrix v%v.%v\n", VERSION_MAJOR, VERSION_MINOR)
 
 	// Load the config
 	cfg, err := loadConfig(cfgfile)
